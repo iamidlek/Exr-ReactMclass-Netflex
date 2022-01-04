0899f14cc4b7b59b85ba221c9efd99adbcf2a1a3
iamidlek <iamidlek@naver.com>
2022-01-04 20:53:34 +0900

    feat: set base code and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import Search from "./routes/Search";
+import Tv from "./routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
