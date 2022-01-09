48f810ceee2b91b4fae1c5982d5dd59d04c29240
iamidlek <iamidlek@naver.com>
2022-01-09 19:28:19 +0900

    motionvalues2

---
+++ study/Motionvalues2.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
+// import { useEffect } from "react";
+
+const Wrapper = styled(motion.div)`
+  height: 200vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [0, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+
+  // scrollY 좌표 , scrollYProgress 전체 높이 중 현재 위치의 % 정보
+  // const { scrollY, scrollYProgress } = useViewportScroll();
+  // useEffect(() => {
+  //   scrollY.onChange(() => {
+  //     console.log(scrollY.get(), scrollYProgress.get());
+  //   });
+  // }, [scrollYProgress,scrollY]);
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  return (
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+    </Wrapper>
+  );
+}
+export default App;
